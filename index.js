e95773197755c7a409c1c97747ef3b2c122ea5f2
harshapericherla <harsha.pericherla@gmail.com>
2020-05-31 16:55:11 -0400

    testing out

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./graphql_server/schema');
-const resolvers = require('./graphql_server/resolvers');
+const resolver = require('./graphql_server/resolvers');
 
 
 const server = new ApolloServer({
                 typeDefs,
-                resolvers
+                resolver
             });
 
 server.listen(4000);
