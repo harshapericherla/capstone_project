377cc745b6a9e9ee633f76677910d1b90453b167
harshapericherla <harsha.pericherla@gmail.com>
2020-06-28 20:29:37 -0400

    remove dev.js as it contains confedential keys

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://harsha:harsha@cluster0-g6snv.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientId: '1010586786186-ch5ilj92cfuaj040o7dqfm5g7el2sq1a.apps.googleusercontent.com',
-    googleClientSecret: 'si2TgYdsV5aUDp2A_p5sJOud',
-    jwtSecretKey:'UdV5Dps2TgYa'
-};
